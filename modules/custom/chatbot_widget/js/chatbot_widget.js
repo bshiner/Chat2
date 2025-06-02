18d48a482dee635a4b01767e2f2fdc592d8eb454
Brandon <brandonshiner@gmail.com>
2025-06-02 02:12:51 -0400

    fix jquery change

--- modules/custom/chatbot_widget/js/chatbot_widget.js
+++ modules/custom/chatbot_widget/js/chatbot_widget.js
@@ -1,54 +1,56 @@
-(function ($, Drupal) {
+(function ($, Drupal, once) {
   Drupal.behaviors.chatbotWidget = {
     attach: function (context, settings) {
-      const $widget = $('#chatbot-widget', context);
-      const $button = $('.chatbot-button', $widget);
-      const $container = $('.chatbot-container', $widget);
-      const $closeButton = $('.close-chat', $widget);
-      const $input = $('#chatbot-input-field', $widget);
-      const $sendButton = $('#chatbot-send-button', $widget);
-      const $messages = $('.chatbot-messages', $widget);
+      once('chatbot-widget', '#chatbot-widget', context).forEach(function (widget) {
+        const $widget = $(widget);
+        const $button = $('.chatbot-button', $widget);
+        const $container = $('.chatbot-container', $widget);
+        const $closeButton = $('.close-chat', $widget);
+        const $input = $('#chatbot-input-field', $widget);
+        const $sendButton = $('#chatbot-send-button', $widget);
+        const $messages = $('.chatbot-messages', $widget);
 
-      // Toggle chat container visibility
-      $button.once('chatbot-widget').on('click', function () {
-        $container.toggle();
-      });
+        // Toggle chat container visibility
+        $button.on('click', function () {
+          $container.toggle();
+        });
 
-      // Close chat container
-      $closeButton.once('chatbot-widget').on('click', function () {
-        $container.hide();
-      });
+        // Close chat container
+        $closeButton.on('click', function () {
+          $container.hide();
+        });
 
-      // Send message
-      function sendMessage() {
-        const message = $input.val().trim();
-        if (message) {
-          addMessage('user', message);
-          $input.val('');
-          // TODO: Send message to API endpoint
-          // For now, we'll just simulate a response
-          setTimeout(() => {
-            addMessage('bot', 'Thank you for your message. This is a placeholder response.');
-          }, 1000);
+        // Send message
+        function sendMessage() {
+          const message = $input.val().trim();
+          if (message) {
+            addMessage('user', message);
+            $input.val('');
+            // TODO: Send message to API endpoint
+            // For now, we'll just simulate a response
+            setTimeout(() => {
+              addMessage('bot', 'Thank you for your message. This is a placeholder response.');
+            }, 1000);
+          }
         }
-      }
 
-      $sendButton.once('chatbot-widget').on('click', sendMessage);
-      $input.once('chatbot-widget').on('keypress', function (e) {
-        if (e.which === 13) {
-          sendMessage();
-          e.preventDefault();
+        $sendButton.on('click', sendMessage);
+        $input.on('keypress', function (e) {
+          if (e.which === 13) {
+            sendMessage();
+            e.preventDefault();
+          }
+        });
+
+        // Add message to chat
+        function addMessage(sender, text) {
+          const $message = $('<div>')
+            .addClass(`message ${sender}-message`)
+            .text(text);
+          $messages.append($message);
+          $messages.scrollTop($messages[0].scrollHeight);
         }
       });
-
-      // Add message to chat
-      function addMessage(sender, text) {
-        const $message = $('<div>')
-          .addClass(`message ${sender}-message`)
-          .text(text);
-        $messages.append($message);
-        $messages.scrollTop($messages[0].scrollHeight);
-      }
     }
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
